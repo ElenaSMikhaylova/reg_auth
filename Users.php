d70bd5e0c8e06068ed41547e16793bac114feff5
Elena <89309190+ElenaSMikhaylova@users.noreply.github.com>
2021-09-14 08:06:54 +0300

    правки утро 140921
    выключила return и закомментила сессию

---
+++ Users.php
@@ -0,0 +1,38 @@
+<?php
+class User
+{
+    private $name;
+    private $lastname;
+    private $email;
+    private $id;
+
+    function __construct($id, $name, $lastname, $email)
+    {
+        $this->id = $id;
+        $this->id = $name;
+        $this->id = $lastname;
+        $this->id = $email;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+    function getName()
+    {
+        return $this->name;
+    }
+    function getLastname()
+    {
+        return $this->lastname;
+    }
+    function getEmail()
+    {
+        return $this->email;
+    }
+
+    static function addUser()
+    {
+        echo "User added";
+    }
+}
